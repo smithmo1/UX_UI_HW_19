3b568f3a5310d320f76c483d8418d85345e5742a
smithmo1 <morganasmith0@gmail.com>
2021-11-13 01:40:21 -0500

    Changes to CSS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 
 console.log("Your index.js file is loaded correctly!");
 $("#myID").on("click", function (){ console.log ("You clicked on #myID");});
+
+$("#myID").on("click", function(){
+    $(".fadeMe").fadeToggle("slow")
+}
